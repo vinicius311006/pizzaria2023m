5d5abf9aadf8dd9efaf47deb5870318b258bdc8a
vinicius311006 <vinicius311006@gmail.com>
2024-06-05 11:53:05 -0300

    adicionado carrossel

---
+++ js/carrossel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carrossel').slick({
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        infinite: true,
+        speed: 1500,
+        autoplayspeed: 3000
+    });
+  });
